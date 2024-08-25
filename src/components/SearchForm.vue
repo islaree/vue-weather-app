7cbeda43d6d3f35de8356006b51e593f5129511f
daxchx <daxchx.v1@gmail.com>
2024-08-25 22:36:44 +0900

    add: component

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const emit = defineEmits(["handle-search"]);
+
+const cityName = ref("");
+
+const handleSubmit = (e: Event) => {
+  e.preventDefault();
+  emit("handle-search", cityName.value);
+};
+</script>
+
+<template>
+  <form @submit="handleSubmit">
+    <input v-model="cityName" placeholder="都市名を入力してください" />
+    <button>search</button>
+  </form>
+</template>
---
+++ src/components/SearchResult.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { WeatherResponse } from "../types/weather";
+
+defineProps<{
+  data: WeatherResponse | null;
+}>();
+
+const getIconUrl = (icon: string) => {
+  return `http://openweathermap.org/img/wn/${icon}.png`;
+};
+</script>
+
+<template>
+  <div v-if="data">
+    <div>{{ data?.name }}の天気</div>
+    <div>緯度, 軽度：{{ data?.coord.lat }}, {{ data?.coord.lon }}</div>
+    <div v-for="(weather, index) in data?.weather" :key="index">
+      <img :src="getIconUrl(weather.icon)" :alt="weather.description" />
+      <div>main: {{ weather.main }}</div>
+      <div>description: {{ weather.description }}</div>
+    </div>
+  </div>
+  <div v-else>
+    <p>データが見つかりませんでした。</p>
+  </div>
+</template>

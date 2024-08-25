480e566a89981b401b78d16cedd97d8817d1e2f7
daxchx <daxchx.v1@gmail.com>
2024-08-25 22:35:42 +0900

    add: get location

---
+++ src/api/location.ts
@@ -0,0 +1,18 @@
+import { Location, LocationsResponse } from "../types/location";
+
+const apiKey = import.meta.env.VITE_API_KEY;
+const baseUrl = "http://api.openweathermap.org/geo/1.0/direct";
+const limit = 1;
+
+export async function getLocation(query: string): Promise<Location | null> {
+  const res = await fetch(
+    `${baseUrl}?q=${query}&limit=${limit}&appid=${apiKey}`
+  );
+
+  if (!res.ok) {
+    throw new Error("Network response was not ok");
+  }
+
+  const data: LocationsResponse = await res.json();
+  return data.length > 0 ? data[0] : null;
+}

e83a687be633ef88c8c9c95d435862d638acacb5
daxchx <daxchx.v1@gmail.com>
2024-08-25 22:35:08 +0900

    feat: search functionality

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,45 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup lang="ts">
+import { ref } from "vue";
+import { getLocation } from "./api/location";
+import { getWeather } from "./api/weather";
+import SearchForm from "./components/SearchForm.vue";
+import SearchResult from "./components/SearchResult.vue";
+import { WeatherResponse } from "./types/weather";
+
+const data = ref<WeatherResponse | null>(null);
+const error = ref<string | null>(null);
+
+const handleSearch = async (query: string) => {
+  try {
+    const location = await getLocation(query);
+
+    if (location) {
+      const { lat, lon } = location;
+      const weather = await getWeather(lat, lon);
+
+      if (weather) {
+        data.value = weather;
+        error.value = null;
+      } else {
+        error.value = "Weather data not found.";
+      }
+    } else {
+      error.value = "Location not found.";
+    }
+  } catch (error) {
+    console.error("Failed to fetch weather data:", error);
+  }
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <SearchForm @handle-search="handleSearch" />
+  <SearchResult :data="data" />
+  <div v-if="error" class="error-message">{{ error }}</div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style>
+.error-message {
+  color: red;
 }
 </style>
